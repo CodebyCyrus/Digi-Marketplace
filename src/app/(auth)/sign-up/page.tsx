acd847e25eaaa95930ad07f2b2076b9d6f7723c6
PersianDavinci <persiandavinci@gmail.com>
2024-03-08 19:16:00 +0330

    add auth folder on app and add sign-up secoundary style

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,4 +1,7 @@
 import { Icons } from "@/components/Icons";
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 const Page = () => {
   return (
@@ -7,6 +10,18 @@ const Page = () => {
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col items-center space-y-2 text-center">
             <Icons.logo className="h-20 w-20" />
+            <h1 className="text-2xl font-bold">Create an account</h1>
+
+            <Link
+              className={buttonVariants({
+                variant: "link",
+                className: "gap-1.5",
+              })}
+              href="/sign-in"
+            >
+              Already have an account? Sign-in
+              <ArrowRight className="h-4 w-4" />
+            </Link>
           </div>
         </div>
       </div>
