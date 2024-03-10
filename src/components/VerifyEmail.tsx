34fa21e792413e037246ca129765b3ff11d54319
PersianDavinci <persiandavinci@gmail.com>
2024-03-10 13:26:43 +0330

    create VerifyEmail part2

--- src/components/VerifyEmail.tsx
+++ src/components/VerifyEmail.tsx
@@ -1,16 +1,28 @@
 "use client";
 
 import { trpc } from "@/trpc/client";
+import { XCircle } from "lucide-react";
 
 interface VerifyEmailProps {
   token: string;
 }
 
 const VerifyEmail = ({ token }: VerifyEmailProps) => {
-  const {} = 
-    const {} = trpc.auth.verifyEmail.useQuery({
-      token,
-    });
-  };
+  const { data, isLoading, isError } = trpc.auth.verifyEmail.useQuery({
+    token,
+  });
+
+  if (isError) {
+    return (
+      <div className="flex flex-col items-center gap2 ">
+        <XCircle className="h-8 w-8 text-red-600" />
+        <h3 className="font-semibold text-xl">There was a problem</h3>
+        <p className="text-muted-foreground text-sm">
+          This token is not valid or might be expired. Please try again.
+        </p>
+      </div>
+    );
+  }
+};
 
 export default VerifyEmail;
