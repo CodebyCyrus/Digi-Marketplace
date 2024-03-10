699c3becf94ce71ae98658d9d77880009e3c25f9
PersianDavinci <persiandavinci@gmail.com>
2024-03-10 11:53:21 +0330

    configuring email in Users on collections

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -2,7 +2,13 @@ import { CollectionConfig } from "payload/types";
 
 export const Users: CollectionConfig = {
   slug: "users",
-  auth: true,
+  auth: {
+    verify: {
+      generateEmailHTML: ({ token }) => {
+        return `<p>Hi, Please Verify Your Email</p>`;
+      },
+    },
+  },
   access: {
     read: () => true,
     create: () => true,
